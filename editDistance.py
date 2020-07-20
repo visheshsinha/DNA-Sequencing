600a9f3d633ba50156cf909c46f43bf89c1bfcf9
visheshsinha <visheshsinha801@gmail.com>
2020-07-21 01:30:32 +0530

    ✔ Approximate Approach for editDistance

--- editDistance.py
+++ editDistance.py
@@ -40,3 +40,25 @@ def editDistance(x, y):
     
     # Edit distance is the value in the bottom right corner of the matrix
     return D[-1][-1]
+
+# editDistance for Approximate Matching with minimum edits
+
+def editDistance_approx(x, y):
+    D = []
+    for i in range(len(x) + 1):
+        D.append([0] * (len(y)+1))
+
+    for i in range(1, len(x)+1):
+        D[i][0] = i
+
+    for i in range(1, len(x)+1):
+        for j in range(1, len(y)+1):
+            distHor = D[i][j-1] + 1
+            distVer = D[i-1][j] + 1
+            if x[i-1] == y[j-1]:
+                distDiag = D[i-1][j-1]
+            else:
+                distDiag = D[i-1][j-1] + 1
+            D[i][j] = min(distHor, distVer, distDiag)
+    
+    return min(D[:][-1])
--- overlaps.py
+++ overlaps.py
@@ -25,3 +25,24 @@ def naive_overlap_map(reads, k):
         if olen > 0:
             olaps[(a, b)] = olen
     return olaps
+
+# Dynamic Approach for reacting Overlap Graph for k-mer suffix overlap
+
+from collections import defaultdict
+
+def overlap_graph(reads, k):
+    index = defaultdict(set)
+    for read in reads:
+        for i in range(len(read) - k + 1):
+            index[read[i:i+k]].add(read)
+            
+    graph = defaultdict(set)
+    for r in reads:
+        for o in index[r[-k:]]:
+            if r != o :                    # For avoiding counting overlapping the read to itself 
+                if overlap(r, o, k):
+                    graph[r].add(o)
+    edges = 0
+    for read in graph:
+        edges += len(graph[read])
+    return(edges, len(graph))
